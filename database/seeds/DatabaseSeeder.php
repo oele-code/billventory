029282c9b28887b97589b62ad57fa7202e25ca98
Osmell Caicedo <correo.oele@gmail.com>
2024-11-11 11:32:09 -0500

    restore admin values in databaseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      */
     public function run(): void
     {
-        //
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Johnd Doe',
+            'email' => 'johndoe@example.com',
+            'type' => 1,
+        ]);
     }
 }
